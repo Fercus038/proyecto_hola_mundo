2f6a4ecc03b97fb04619fc1b7770fb46be2abc38
Juan Cuscun <crfernando038@gmail.com>
2024-03-12 20:55:01 -0600

    Primer commit

---
+++ Actividad3.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Hola Mundo, Mi nombre es Juan Cuscun<br>";
+echo "El es mi mejor amigo Canino, Chester. <br>";
+
+
+?>
+
+<img src="chester.jpg" alt="Mi Mejor Amigo Canino" width="400" height="500">
